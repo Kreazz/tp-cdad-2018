de800693c51937ed5d933f1e9fd5b43cb50d1ec0
qmayer <qmayer@etu.unistra.fr>
2019-01-11 16:49:18 +0100

    Téléversement d’un fichier fait

--- api/controllers/CowsayController.js
+++ api/controllers/CowsayController.js
@@ -19,9 +19,11 @@ module.exports = {
     let sentence = "Random Message";
     if(s.length > 0) {
       sentence = s[0].sentence;
+      picture = s[0].picture;
     }
     return res.view('cowsay', { cow: cowsay.say({
       f: process.env.COW || 'stegosaurus',
+      picture : picture,
       text : sentence,
       e : 'oO',
       T : 'U '
@@ -32,9 +34,40 @@ module.exports = {
     return res.view('add');
   },
 
-  create: async function(req, res) {
-    await Sentences.create({ sentence: req.param('sentence') });
-    return res.redirect('/say');
+uploadFile: function (req, res) {
+    req.file('file').upload({
+      adapter: require('skipper-better-s3'),
+      key: 'AKIAJOCSBD4KTGNIE2YQ',
+      secret: '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',
+      bucket: 'lp-cdad-2018',
+      region: 'eu-west-3',
+      s3params: { ACL: 'public-read'},
+      onProgress: progress => sails.log.verbose('Upload progress:', progress)
+    }, function (err, filesUploaded) {
+      if (err) return res.serverError(err);
+      return res.ok({
+        saved_files: filesUploaded,
+        location: filesUploaded[0].extra.Location,
+	textParams: req.allParams()
+      });
+    });
   },
+
+  create:  async function (req, res) {
+    await req.file('file').upload({
+      adapter: require('skipper-better-s3'),
+      key: 'AKIAJOCSBD4KTGNIE2YQ',
+      secret: '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',
+      bucket: 'lp-cdad-2018',
+      region: 'eu-west-3',
+      s3params: { ACL: 'public-read'},
+      onProgress: progress => sails.log.verbose('Upload progress:', progress)
+    }, async function (err, filesUploaded) {
+      if (err) return res.serverError(err);
+      await Sentences.create({ sentence: req.param('sentence'), picture: filesUploaded[0].extra.Location });
+      return res.redirect('/say');
+    });
+
+   },
 };
 
